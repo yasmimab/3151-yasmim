5f41ccdeac108ac0f812663b83bba0f1fe65b5d3
yasmimab <yasmim.barbosa.estudante@eteot.rio.br>
2023-06-30 10:08:24 -0300

    oi

---
+++ siscrud/base/sidebar2.php
@@ -0,0 +1,36 @@
+<?php
+$nivel_necessario = 2;
+include "base/testa_nivel.php";
+?>
+<div class="sidebar close">
+    <div class="logo-details">
+      <i class='bx bxl-c-plus-plus'></i>
+      <span class="logo_name">Siscrud</span>
+    </div>
+    <ul class="nav-links">
+      <li>
+        <a href="?page=home">
+          <i class='bx bx-home-alt' ></i>
+          <span class="link_name">Home</span>
+        </a>
+      </li>
+      <li>
+        <a href="?page=lista_alu">
+          <i class='bx bxs-graduation'></i>
+          <span class="link_name">Aluno</span>
+        </a>
+      </li>
+      <li>
+        <a href="?page=lista_disc">
+          <i class='bx bxs-book'></i>
+          <span class="link_name">Disciplina</span>
+        </a>
+      </li>
+      <li>
+      <a href="?page=logout">
+      <i class='bi bi-box-arrow-right'>->]</i>
+      <span class="link_name">Logout</span>
+      </a>
+      </li>
+</ul>
+  </div>
---
+++ siscrud/sis/alunos/excluir_alu.php
@@ -0,0 +1,15 @@
+﻿<?php
+$matricula = $_GET['matricula'];
+ 
+$sql = "delete from aluno where mat_alu = '$matricula';"; 
+
+$resultado = mysqli_query($con, $sql)or die(mysqli_error());
+
+if ($resultado) {
+    header('Location: \siscrud/index.php?page=lista_alu&msg=3');
+    mysqli_close($con);
+}else{
+    header('Location: \siscrud/index.php?page=lista_alu&msg=4');
+    mysqli_close($con);
+}
+?>
